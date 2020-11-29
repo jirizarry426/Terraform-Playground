16c45b28b2930e8611c0b6c0a046a96ef8e20c56
Joshua Irizarry <jirizarry0426@gmail.com>
2020-11-29 14:50:53 -0500

    update to config json

--- Windows/JI-TF-PG.tf
+++ Windows/JI-TF-PG.tf
@@ -36,15 +36,16 @@ Set-ExecutionPolicy -executionpolicy unrestricted
 New-Item -ItemType directory -Path C:\install_tableau
 cd C:\install_tableau
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe -OutFile "Python3.9.0.exe"
-Invoke-WebRequest https://downloads.tableau.com/esdalt/2020.2.3/TableauServer-64bit-2020-2-3.exe -OutFile "Tableau_Server-2020.2.3.exe"
-Invoke-WebRequest https://downloads.tableau.com/esdalt/2020.2.3/TableauServerTabcmd-64bit-2020-2-3.exe -OutFile "Tableau_Server_TabCMD-2020.2.3.exe"
-Invoke-WebRequest https://downloads.tableau.com/esdalt/2020.2.2/tableau_powertools/Tabcmt-32bit-2020-2-2.exe -OutFile "Tableau_Server_TabCMT-2020.2.2.exe"
 Invoke-WebRequest https://raw.githubusercontent.com/tableau/server-install-script-samples/master/windows/tsm/SilentInstaller/SilentInstaller.py -OutFile "Tableau_Silent_Installer.py"
 Invoke-WebRequest https://raw.githubusercontent.com/jirizarry426/Terraform-Playground/main/Windows/bootstrap.json -OutFile "bootstrap.json"
 Invoke-WebRequest https://raw.githubusercontent.com/jirizarry426/Terraform-Playground/main/Windows/config.json -OutFile "config.json"
 Invoke-WebRequest https://raw.githubusercontent.com/jirizarry426/Terraform-Playground/main/Windows/secrets.json -OutFile "secrets.json"
 Invoke-WebRequest https://raw.githubusercontent.com/jirizarry426/Terraform-Playground/main/Windows/reg_templ.json -OutFile "reg.json"
+Invoke-WebRequest https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe -OutFile "Python3.9.0.exe"
+Invoke-WebRequest https://downloads.tableau.com/esdalt/2020.2.3/TableauServer-64bit-2020-2-3.exe -OutFile "Tableau_Server-2020.2.3.exe"
+Invoke-WebRequest https://downloads.tableau.com/esdalt/2020.2.3/TableauServerTabcmd-64bit-2020-2-3.exe -OutFile "Tableau_Server_TabCMD-2020.2.3.exe"
+Invoke-WebRequest https://downloads.tableau.com/esdalt/2020.2.2/tableau_powertools/Tabcmt-32bit-2020-2-2.exe -OutFile "Tableau_Server_TabCMT-2020.2.2.exe"
+.\Tableau_Silent_Installer.py --bootstrapFile C:\install_tableau\bootstrap.json
 Set-ExecutionPolicy -executionpolicy restricted 
 </powershell> 
 EOF
@@ -56,6 +57,16 @@ EOF
   vpc_security_group_ids = [
     "sg-c43d66a6"
   ]
+
+  resource "aws_ebs_volume" "tableau" {
+  availability_zone = "us-west-2a"
+  size              = 100
+
+  tags = {
+    Name = "Tableau"
+  }
+}
+
 }
 
 
